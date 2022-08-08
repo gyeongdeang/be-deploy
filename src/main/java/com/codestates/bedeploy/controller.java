17ceaf73e395a6fbaa0dda1025492970974bebf7
gyeongdeang <joohyeon0286@gmail.com>
2022-08-09 05:49:28 +0900

    마지막

--- src/main/java/com/codestates/bedeploy/controller.java
+++ src/main/java/com/codestates/bedeploy/controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping("/")
     public String index(){
-        return "잘했어";
+        return "잘했어!!";
     }
 }
